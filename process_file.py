01c37d776b50a6674a17f69cc1336ccee405f015
Alexey Zankevich <alex.zankevich@gmail.com>
2018-08-16 23:32:54 +0300

    Added fake file processing script

--- process_file.py
+++ process_file.py
@@ -1,16 +1,22 @@
 import argparse
+import cv2
+
+from cnn import get_model
 
 
-def process_file(wieghts_file, input_file, output_file):
-    pass
+def process_file(weights_file, input_file, output_file):
+    model = get_model()
+    model.load_weights(weights_file)
+
+    img = cv2.imread(input_file, cv2.IMREAD_GRAYSCALE)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--weights', dest='weights_file', help='Save weights to file', default='weights.h5')
     parser.add_argument('-i', '--input-file', dest='input_file', help='Input file to process', required=True)
-    parser.add_argument('-o', '--output-file', dest='output_file', help='Processed output file', required=True)
+    parser.add_argument('-o', '--output-file', dest='output_file', help='Processed output file', default='output.png')
+    parser.add_argument('-w', '--weights', dest='weights_file', help='Save weights to file', default='weights.h5')
 
     args = parser.parse_args()
 
-    process_file()
+    process_file(args.weights_file, args.input_file, args.output_file)
