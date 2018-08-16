fe18f7b8c65cf673e64d52db72b03726f1ad6e5a
Alexey Zankevich <alex.zankevich@gmail.com>
2018-08-16 17:45:36 +0300

    Flattered y train data

--- train_cnn.py
+++ train_cnn.py
@@ -12,22 +12,38 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 
 
-class TileLoader:
+class XTileLoader:
+    """
+    Load square tiles with channel
+    """
     def __init__(self, tiles_dir, tile_size):
         self.tiles_dir = tiles_dir
         self.tile_size = tile_size
 
-    def load(self):
-        files = sorted(os.listdir(self.tiles_dir))
+    def get_shape(self):
+        return (self.tile_size, self.tile_size, 1)
 
-        train_data = np.zeros((len(files), self.tile_size, self.tile_size, 1))
-        for i, fname in enumerate(files):
-            train_data[i] = cv2.imread(os.path.join(self.tiles_dir, fname), cv2.IMREAD_GRAYSCALE)\
-                .reshape((self.tile_size, self.tile_size, 1))
+    def load(self):
+        tiles_list = sorted(os.listdir(self.tiles_dir))
+        shape = (len(tiles_list),) + self.get_shape()
+        train_data = np.zeros(shape)
+        for i, fname in enumerate(tiles_list):
+            path = os.path.join(self.tiles_dir, fname)
+            tile = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            tile = tile.reshape(self.get_shape())
+            train_data[i] = tile
             train_data = train_data.astype('float32')
             train_data /= 255
 
         return train_data
+
+
+class YTileLoader(XTileLoader):
+    """
+    Load tile as flat array without channel
+    """
+    def get_shape(self):
+        return (self.tile_size * self.tile_size,)
 
 
 def cache_train_data(filename):
@@ -64,11 +80,14 @@ def load_data(x_path, y_path):
 
     assert raw_files == clean_files, 'X/Y files are not the same'
 
-    print('Loading x train data')
-    x_train = TileLoader(x_path, 64).load()
-
     print('Loading y train data')
-    y_train = TileLoader(y_path, 32).load()
+    y_train = YTileLoader(y_path, 32).load()
+
+    print('Loading x train data')
+    x_train = XTileLoader(x_path, 64).load()
+
+    print(y_train)
+    # print(x_train)
 
     return x_train, y_train
 
@@ -79,17 +98,15 @@ def main():
     x_train, y_train = load_data('samples-raw', 'samples-clean')
 
     print('Creating CNN')
-    # 7. Define model architecture
     model = Sequential()
 
-    model.add(Convolution2D(64, (3, 3), activation='relu', input_shape=(64, 64, 1)))
-    model.add(Convolution2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(32, (3, 3), activation='relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(1024, activation='relu'))
+    model.add(Convolution2D(20, (3, 3), activation='relu', input_shape=(64, 64, 1), padding='same'))
+    # model.add(MaxPooling2D((2, 2)))
+    # model.add(Convolution2D(20, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+    # model.add(MaxPooling2D((2, 2)))
+    # model.add(Convolution2D(20, (3, 3), activation='relu', input_shape=(64, 64, 1)))
+    # model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
 
     # 8. Compile model
     model.compile(loss='mean_absolute_percentage_error', optimizer='adam', metrics=['accuracy'])
