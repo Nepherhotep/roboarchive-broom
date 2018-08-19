27d2faa7726b81d55dce113dda412c6bab3851ae
Kirill Pinchuk <cybergrind@gmail.com>
2018-08-19 16:01:22 +0300

    Optimizer

--- cnn/unet.py
+++ cnn/unet.py
@@ -84,5 +84,6 @@ def UNet(
 class UnetCNN(BaseCNN):
     def get_model(self):
         model = UNet((256, 256, 1))
-        model.compile(optimizer=Adam(lr=1e-4), loss="binary_crossentropy", metrics=["accuracy"])
+        # model.compile(optimizer=Adam(lr=1e-4), loss="binary_crossentropy", metrics=["accuracy"])
+        model.compile(optimizer='nadam', loss="binary_crossentropy", metrics=["accuracy"])
         return model
