44080de127ac286f16758fc68cc32c1162049819
Alexey Zankevich <alex.zankevich@gmail.com>
2018-08-23 11:13:01 +0300

    Updated slice tile function tests

--- tests/test_split_image.py
+++ tests/test_split_image.py
@@ -14,21 +14,38 @@ def img():
 
 
 def test_slice_tile(img):
-    tile, (h, w) = slice_tile(img, 0, 0, 3, 0)
+    tile, (h, w) = slice_tile(img, 0, 0, tile_size=3, padding=0)
 
     expected = np.array([[0,  1,  2],
                          [10, 11, 12],
                          [20, 21, 22]], np.uint8)
 
+    assert h == 3, h
+    assert w == 3, w
     assert np.array_equal(tile, expected), tile
 
 
 def test_slice_tile2(img):
-    tile, (h, w) = slice_tile(img, 0, 0, 3, 1, bg_color=7)
+    tile, (h, w) = slice_tile(img, 0, 0, tile_size=3, padding=1, bg_color=7)
     expected = np.array([[7, 7, 7, 7, 7],
                          [7, 0, 1, 2, 3],
                          [7, 10, 11, 12, 13],
                          [7, 20, 21, 22, 23],
                          [7, 30, 31, 32, 33]], np.uint8)
 
+    assert h == 4, h
+    assert w == 4, h
+    assert np.array_equal(tile, expected), tile
+
+
+def test_slice_tile3(img):
+    tile, (h, w) = slice_tile(img, 3, 3, tile_size=3, padding=1, bg_color=7)
+    expected = np.array([[88, 89,  7,  7,  7],
+                         [98, 99,  7,  7,  7],
+                         [7,  7,  7,  7,  7],
+                         [7,  7,  7,  7,  7],
+                         [7,  7,  7,  7,  7]], np.uint8)
+
+    assert h == 2, h
+    assert w == 2, w
     assert np.array_equal(tile, expected), tile
