0000b385c9821471351c640d9a45c3c76b77de0a
Alexey Zankevich <alex.zankevich@gmail.com>
2018-08-30 16:53:20 +0300

    Added comments

--- extract_nist_text.py
+++ extract_nist_text.py
@@ -38,19 +38,22 @@ def extract_text(args, input_path, output_path):
     if approx.shape[0] == 4:
         mask = np.zeros(grayscale.shape, np.uint8)
 
+        # fill bounding box
         cv2.fillConvexPoly(mask, cnt, 255)
 
+        # reduce bounding box size to get rid of lines
+        cv2.polylines(mask, [cnt], True, (0, 0, 0), 20)
+
+        # save inverted matrix to add white borders in future
         inverted = 255 - mask
 
-        # display(inverted)
-
+        # copy bounding box to destination image
         dst = cv2.bitwise_and(grayscale, grayscale, mask=mask)
 
+        # restore white color around bounding box
         dst = inverted + dst
-        print(dst[450:451, 500:510])
-        # cv2.imwrite(output_path, dst)
 
-        display(dst)
+        cv2.imwrite(output_path, dst)
         return True
 
     else:
@@ -65,7 +68,7 @@ def main(args):
     lst.sort()
 
     skipped = 0
-    for i, f in enumerate(lst[5:6]):
+    for i, f in enumerate(lst[:]):
         input_path = os.path.join(args.data_dir, 'nist_orig', f)
         output_path = os.path.join(args.data_dir, 'text_extracted', f)
 
